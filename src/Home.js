23057380e3989f1ef5af5a07f086f0635fa13991
unknown <Multimedia@MULTIMEDIA-PC.usiu.ac.ke>
2022-02-24 15:47:11 +0300

    useEffect in react

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
 
@@ -8,6 +8,8 @@ const Home = () => {
         { title: 'Hacking Roadmap', body: 'Lorem ipsum dolor sit amet.....', author: 'Norbert', id: 3 }
     ])
 
+    const [name, setName] = useState('Nobby')
+
 
     // const handleClick = ()=>{
     //     seName('Gudah')
@@ -16,15 +18,28 @@ const Home = () => {
 
     //function to delete an item using filter method
     const deleteBlog = (id) =>{
-        console.log("deleted");
         const newBlogs = blogs.filter((blog)=>blog.id !==id);
         seBlogs(newBlogs)
     }
 
+    //useEffect with dependencies. 
+    //N.B=Use effect is a function that runs after every render of a page
+    useEffect(()=>{
+        console.log('Use Effect run')
+        console.log(name)
+    }, [name])
+
+
+    //UseEffect without dependencies
+    // useEffect(()=>{
+    //     console.log('Use Effect run')
+    //     console.log(name)
+    // })
     return (
         <div>
             <BlogList blogs={blogs} title="All Blogs" deleteBlog = {deleteBlog}/>
-            {/* <BlogList blogs={blogs.filter((blog)=>blog.author==='Norbert')} title="Norbert's Blogs"/> */}
+            <p>{name}</p>
+            <button onClick={()=>setName('Norbert')}>change name</button>
         </div>
     );
 }

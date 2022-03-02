ece5123db12c2ff065f7c4e4c38492989633fc2e
Main Lab <mlab@usiu.ac.ke>
2022-03-02 15:28:41 +0300

    Error handling

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,7 +1,4 @@
 const BlogList = ({blogs, title}) => {
-    // const blogs = props.blogs
-    // const title = props.title
-    // const deleteBlog = props.deleteBlog
     return (
         <div>
             <h2>{title}</h2>
@@ -10,7 +7,7 @@ const BlogList = ({blogs, title}) => {
                     <h2>{blog.title}</h2>
                     <p>Written by: {blog.author}</p>
                 </div>
-            ))}
+            ))} 
         </div>
     );
 }
--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
     const [blogs, seBlogs] = useState(null)
     const [isLoading, setIsLoading] = useState(true)
     const [name, setName] = useState('Nobby')
+    const [err, setErr] = useState(null)
 
 
     // const handleClick = ()=>{
@@ -21,11 +22,19 @@ const Home = () => {
        setTimeout(()=>{
         fetch('http://localhost:8000/blogs')
         .then(res=>{
+            if(!res.ok){
+                throw Error('Cannot Fetch the data. Please try again')
+            }
             return res.json();
         })
         .then(data=>{
-            console.log(data);
             seBlogs(data)
+            setIsLoading(false)
+            setErr(null)
+        })
+        .catch(err =>{
+            // console.log(err.message)
+            setErr(err.message)
             setIsLoading(false)
         })
        }, 1000)
@@ -40,6 +49,7 @@ const Home = () => {
     return (
         <div>
             {/* conditional output */}
+            {err && <div>{err}</div>}
             {isLoading && <div>Loading... </div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>

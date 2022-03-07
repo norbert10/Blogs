931d56cb37d4423e22cc0ec83508151a4476bdcb
Main Lab <mlab@usiu.ac.ke>
2022-03-07 14:30:39 +0300

    useEffect cleanup to prevent error when one moves quickly btween different links

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,8 +4,11 @@ const useFetch = (url) => {
     const [isLoading, setIsLoading] = useState(true)
     const [err, setErr] = useState(null)
     useEffect(() => {
+
+        //To abort fetch
+        const abortCont = new AbortController()
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
                 .then(res => {
                     if (!res.ok) {
                         throw Error('Cannot Fetch the data. Please try again')
@@ -18,11 +21,17 @@ const useFetch = (url) => {
                     setErr(null)
                 })
                 .catch(err => {
+                    if(err.name === 'AbortError'){
+                        console.log('Fetch aborted');
+                    }else{
+                        setErr(err.message)
+                        setIsLoading(false)
+                    }
                     // console.log(err.message)
-                    setErr(err.message)
-                    setIsLoading(false)
                 })
-        }, 1000)
+        }, 1000);
+
+        return () => abortCont.abort()
     }, [url])
 
     return { data, isLoading, err }

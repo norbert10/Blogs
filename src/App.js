e9784fd370e0668452b6c01cbf31aa16af6cbe80
Main Lab <mlab@usiu.ac.ke>
2022-03-07 14:17:44 +0300

    Router links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Create from './Create';
 
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className='content'>
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className='content'> 
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/create'component={Create} />
+            </Switch>  
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="navbar-container">
             <h1>Nobby Blogs</h1>
             <div className="nav-links">
-                <a href="#">Home</a>
-                <a href="#">New Blogs</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blogs</Link>
                 {/* <a href="#">About</a> */}
-                <a href="#">Contacts</a>
+
+                {/* <a href="/">Home</a>
+                <a href="/create">New Blogs</a>
+                <a href="#">Contacts</a> */}
 
             </div>
         </nav>

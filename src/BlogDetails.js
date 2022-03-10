5f8df91a13078d15488853d8571c2252ef79ad40
Main Lab <mlab@usiu.ac.ke>
2022-03-10 16:36:16 +0300

    deleting a blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const {data: blog, err, isLoading} = useFetch('http://localhost:8000/blogs/' + id)
+    const history = useHistory();
+
+    const handleDelete =()=>{
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: 'DELETE'
+        }).then(()=>{
+            history.push('/')
+        })
+    }
     return ( 
         <div className="blog-details">
             {isLoading && <div>Loading...</div>}
@@ -13,6 +22,7 @@ const BlogDetails = () => {
                 <h2>{blog.title}</h2>
                 <p>Written by: {blog.author}</p>
                 <div>{blog.body}</div>
+                <button onClick={handleDelete}>delete</button>
             </article>
             )}
         </div>
